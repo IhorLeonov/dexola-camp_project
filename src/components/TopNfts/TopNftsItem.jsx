ba99af0a9a46992750b4c4c05710cf03caaa1b24
Ihor Leonov <ihorleonow@gmail.com>
2023-09-03 15:31:56 +0300

    ADD | styles to top-nfts section

--- src/components/TopNfts/TopNftsItem.jsx
+++ src/components/TopNfts/TopNftsItem.jsx
@@ -3,32 +3,45 @@ import s from "./TopNfts.module.scss";
 export const TopItem = ({ name, rarity, total, won, price, avatar }) => {
   return (
     <li className={s.nfts_grid_item}>
-      <div className={s.nfts_grid}>
+      <table className={s.nfts_grid}>
         {/* header */}
-        <div className={s.nfts_grid_header}>
-          <div className={s.nfts_grid_void}></div>
-          <div className={s.nfts_grid_name}>NFT Name</div>
-          <div className={s.nfts_grid_rarity}>
-            Rarity <span className={s.nfts_grid_level}>Level</span>
-          </div>
-          <div className={s.nfts_grid_total}>Total games</div>
-          <div className={s.nfts_grid_won}>Games Won</div>
-          <div className={s.nfts_grid_price}>Price (ETH)</div>
-        </div>
+        <tr className={s.nfts_grid_header}>
+          <th className={s.nfts_grid_void}>
+            <span className={s.nfts_grid_header_title} />
+          </th>
+          <th className={s.nfts_grid_name}>
+            <span className={s.nfts_grid_header_title}>NFT Name</span>
+          </th>
+          <th className={s.nfts_grid_rarity}>
+            <span className={s.nfts_grid_header_title}>
+              Rarity
+              <span className={s.nfts_grid_level}>Level</span>
+            </span>
+          </th>
+          <th className={s.nfts_grid_total}>
+            <span className={s.nfts_grid_header_title}>Total games</span>
+          </th>
+          <th className={s.nfts_grid_won}>
+            <span className={s.nfts_grid_header_title}>Games Won</span>
+          </th>
+          <th className={s.nfts_grid_price}>
+            <span className={s.nfts_grid_header_title}>Price (ETH)</span>
+          </th>
+        </tr>
         {/* body */}
-        <div className={s.nfts_grid_body}>
-          <div className={s.nfts_grid_image_box}>
+        <tr className={s.nfts_grid_body}>
+          <td className={s.nfts_grid_image_box}>
             <img className={s.nfts_grid_image} src={avatar} alt="NFT picture" />
-          </div>
-          <div className={s.nfts_grid_name_val}>{name}</div>
-          <div className={s.nfts_grid_rarity_val}>{rarity}</div>
-          <div className={s.nfts_grid_total_val}>{total}</div>
-          <div className={s.nfts_grid_won_val}>{won}</div>
-          <div className={s.nfts_grid_price_val}>
+          </td>
+          <td className={s.nfts_grid_name_val}>{name}</td>
+          <td className={s.nfts_grid_rarity_val}>{rarity}</td>
+          <td className={s.nfts_grid_total_val}>{total}</td>
+          <td className={s.nfts_grid_won_val}>{won}</td>
+          <td className={s.nfts_grid_price_val}>
             {price} <span className={s.nfts_grid_cur}>ETH</span>
-          </div>
-        </div>
-      </div>
+          </td>
+        </tr>
+      </table>
     </li>
   );
 };
