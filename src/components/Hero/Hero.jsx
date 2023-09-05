adfebb0b3866c601b4d298c24ad8612c9c3e56be
Ihor Leonov <ihorleonow@gmail.com>
2023-09-05 17:32:56 +0300

    FIX | hero animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,39 +11,62 @@ export const Hero = () => {
       indexFirst >= heroLeftImages.length - 1
         ? setIndexFirst(0)
         : setIndexFirst(indexFirst + 1);
-    }, 5900);
+    }, 4000);
     return () => clearInterval(interval);
   }, [indexFirst]);
 
   useEffect(() => {
-    // const timeout = setTimeout(() => {
     const secondInterval = setInterval(() => {
       indexSecond >= heroRightImages.length - 1
         ? setIndexSecond(0)
         : setIndexSecond(indexSecond + 1);
-    }, 4900);
+    }, 5500);
     return () => clearInterval(secondInterval);
-    // }, 2500);
-    // return () => clearTimeout(timeout);
   }, [indexSecond]);
+
+  const addClassToFirstImage = (idx) => {
+    return idx === indexFirst ? s.hero_image_visible : "";
+  };
+
+  const addClassToSecondImage = (idx) => {
+    return idx === indexSecond ? s.hero_image_visible : "";
+  };
 
   return (
     <section className={s.hero_section}>
       <div className={s.hero_wrapper}>
         <div className={s.hero_img_list + " " + s.hero_container}>
           <div className={s.hero_img_container}>
-            <img
-              className={s.hero_image + " " + s.hero_image_left}
-              src={heroLeftImages[indexFirst]}
-              alt="NFT picture"
-            />
+            {heroLeftImages.map((path, idx) => (
+              <img
+                key={idx}
+                className={
+                  s.hero_image +
+                  " " +
+                  s.hero_image_left +
+                  " " +
+                  addClassToFirstImage(idx)
+                }
+                src={path}
+                alt="NFT picture"
+              />
+            ))}
           </div>
           <div className={s.hero_img_container}>
-            <img
-              className={s.hero_image + " " + s.hero_image_right}
-              src={heroRightImages[indexSecond]}
-              alt="NFT picture"
-            />
+            {heroRightImages.map((path, idx) => (
+              <img
+                key={idx}
+                className={
+                  s.hero_image +
+                  " " +
+                  s.hero_image_right +
+                  " " +
+                  addClassToSecondImage(idx)
+                }
+                src={path}
+                alt="NFT picture"
+              />
+            ))}
           </div>
         </div>
 
