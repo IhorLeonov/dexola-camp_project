d01f401e41727b55f88acbb6a812f1a6c2fe7c49
Ihor Leonov <ihorleonow@gmail.com>
2023-09-06 06:26:04 +0300

    UPD | additional animation

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -1,23 +1,45 @@
 import s from "./Animation.module.scss";
+import { useInView } from "react-intersection-observer";
 
 export const Animation = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
+
+  //   const addClass = () => {
+  //     inView ? "show" : "";
+  //   };
+
+  inView && console.log("congratulations!");
+
   return (
-    <section className={s.animation}>
-      <div className={s.animation_back}></div>
-      <div className={s.animation_ellipse}>
-        <h3 className={s.animation_sub_title}>
-          Explore Cyberpunk Space Adventures in
-        </h3>
-        <h2 className={s.animation_title}>StarRunner ecosystem</h2>
-      </div>
-      <a href="#features">
-        <div className={s.animation_pointer}>
-          <div className={s.animation_pointer_arrow} />
-          <div className={s.animation_pointer_arrow} />
-          <div className={s.animation_pointer_arrow} />
-          <div className={s.animation_pointer_arrow} />
+    <section ref={ref} className={s.section}>
+      <div className={s.animation + " " + `${inView && "show"}`}>
+        <div className={s.animation_back}></div>
+        <div className={s.animation_big_ellipse}>
+          <h3 className={s.animation_sub_title}>
+            Explore Cyberpunk Space Adventures in
+          </h3>
+          <h2 className={s.animation_title}>StarRunner ecosystem</h2>
         </div>
-      </a>
+        <div
+          className={s.animation_ellipse + " " + s.animation_ellipse_vertical}
+        />
+        <div className={s.animation_ellipse} />
+        <div className={s.animation_ellipse} />
+        <div className={s.animation_ellipse} />
+        <div className={s.animation_ellipse} />
+        <div className={s.animation_ellipse} />
+
+        <a href="#features">
+          <div className={s.animation_pointer}>
+            <div className={s.animation_pointer_arrow} />
+            <div className={s.animation_pointer_arrow} />
+            <div className={s.animation_pointer_arrow} />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
